56d24bd47193271fe590dcd2d3a2a863c11a12b5
Peter Miklosko <peter.budo@gmail.com>
2021-03-03 10:21:34 +0000

    Working with tutorial https://developer.android.com/jetpack/compose/tutorial

--- app/src/main/java/com/peter/compose/MainActivity.kt
+++ app/src/main/java/com/peter/compose/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.peter.compose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            NewsStory()
+        }
     }
+}
+
+@Composable
+fun NewsStory() {
+    Column (
+        modifier = Modifier.padding(16.dp)
+            ){
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
+
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    NewsStory()
 }

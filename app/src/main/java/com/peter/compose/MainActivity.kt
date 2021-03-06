2a63e3ae33528e0a41c0dbc7c1230ccdb53dbcbb
Peter Miklosko <peter.budo@gmail.com>
2021-03-06 13:14:22 +0000

    Finished basic tutorial

--- app/src/main/java/com/peter/compose/MainActivity.kt
+++ app/src/main/java/com/peter/compose/MainActivity.kt
@@ -3,11 +3,18 @@ package com.peter.compose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -22,14 +29,38 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun NewsStory() {
-    Column (
-        modifier = Modifier.padding(16.dp)
-            ){
-        Text("A day in Shark Fin Cove")
-        Text("Davenport, California")
-        Text("December 2018")
+    MaterialTheme {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(Modifier.height(16.dp))
+            Text(
+                "A day wandering through the sandhills " +
+                        "in Shark Fin Cove, and a few of the " +
+                        "sights I saw",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                "Davenport, California",
+                style = typography.body2
+            )
+            Text(
+                "December 2018",
+                style = typography.body2
+            )
+        }
     }
-
 }
 
 @Preview

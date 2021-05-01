e45e262163caab87bad724ee9a9d5b459f109194
Peter Miklosko <peter.budo@gmail.com>
2021-05-01 16:17:59 +0100

    Completed layouts in Compose

---
+++ app/src/main/java/com/peter/compose/LayoutsCodelab.kt
@@ -0,0 +1,130 @@
+package com.peter.compose
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.peter.compose.ui.LayoutsCodelabTheme
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+@Composable
+fun LayoutsCodelab() {
+    Scaffold {
+        innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+        StaggeredGrid(modifier = modifier) {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    LayoutsCodelabTheme {
+        LayoutsCodelab()
+    }
+}
+
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowMaxHeights = IntArray(rows) { 0 }
+
+        val placeables = measurables.mapIndexed { index, measurable ->
+            val placeable = measurable.measure(constraints)
+
+            val row = index % rows
+            rowWidths[row] = rowWidths[row] + placeable.width
+            rowMaxHeights[row] = kotlin.math.max(rowMaxHeights[row], placeable.height)
+
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+        val height = rowMaxHeights.sumBy { it }.coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) {0}
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i-1] + rowMaxHeights[i-1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) {0}
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card (
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ChipPreview() {
+    LayoutsCodelabTheme {
+        Chip(text = "Hi there")
+    }
+}
--- app/src/main/java/com/peter/compose/MainActivity.kt
+++ app/src/main/java/com/peter/compose/MainActivity.kt
@@ -3,15 +3,9 @@ package com.peter.compose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.peter.compose.ui.LayoutsCodelabTheme
 
 class MainActivity : AppCompatActivity() {
@@ -19,42 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutsCodelabTheme {
-                LayoutCodelab()
+                LayoutsCodelab()
             }
         }
     }
 }
 
 @Composable
-fun LayoutCodelab() {
-    Scaffold(
-        topBar = {
-            TopAppBar(title = {
-                Text(text = "LayoutCodelab")
-            },
-            actions = {
-                IconButton(onClick = { /*doSomething()*/ }) {
-                    Icon(Icons.Filled.Favorite, contentDescription = null)
-                }
-            })
-        }
-    ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
-    }
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
+@Preview(showBackground = true)
 @Composable
-fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layout codelab")
-    }
-}
-
-@Preview
-@Composable
-fun LayoutCodelabPreview() {
+fun DefaultPreview() {
     LayoutsCodelabTheme {
-        LayoutCodelab()
+        Greeting("Android")
     }
 }
